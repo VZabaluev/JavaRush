cc31dfc438ff581858eb274b68740f649eafe8b6
VZabaluev <noname545@mail.ru>
2022-11-06 23:15:18 +0300

    глобальные константы

--- src/level7/task0615/Earth.java
+++ src/level7/task0615/Earth.java
@@ -9,9 +9,9 @@ package level7.task0615;
 //        •	Переменную equatorLength класса Earth нужно переделать в глобальную переменную.
 
 public class Earth {
-    String name = "Земля";
-    double square = 510_100_000;
-    long population = 7_594_000_000L;
-    long equatorLength = 40_075_696;
+    public static final String NAME = "Земля";
+    public static final double SQUARE = 510_100_000;
+    public static final long POPULATION = 7_594_000_000L;
+    public static final long EQUATOR_LENGTH = 40_075_696;
 }
 
