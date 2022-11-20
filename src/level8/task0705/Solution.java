d45efc0e0d5601afd92208fc4156e6e0cbf704f9
VZabaluev <noname545@mail.ru>
2022-11-20 22:15:56 +0300

    Бесконечность

---
+++ src/level8/task0705/Solution.java
@@ -0,0 +1,14 @@
+package level8.task0705;
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+
+        div(0.0,100.0);
+        div(0.0,-100.0);
+    }
+
+    public static void div(double a, double b){
+        System.out.println(b/a);
+    }
+}
