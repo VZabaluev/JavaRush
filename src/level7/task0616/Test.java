b674244f6c8397d0458ea5794f45afb1fc31da09
VZabaluev <noname545@mail.ru>
2022-11-20 22:29:48 +0300

    Затенение переменных

---
+++ src/level7/task0616/Test.java
@@ -0,0 +1,9 @@
+package level7.task0616;
+
+public class Test {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.setPosition("yuy");
+
+    }
+}
