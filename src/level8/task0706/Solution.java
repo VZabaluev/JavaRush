fdb37d6e8c8461e57a66dee739ed35896e7a2019
VZabaluev <noname545@mail.ru>
2023-01-08 21:23:11 +0300

    Странное деление

---
+++ src/level8/task0706/Solution.java
@@ -0,0 +1,26 @@
+package level8.task0706;
+
+/*
+Странное деление
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        div(0.0,0.0);
+    }
+
+    public static void div(double a, double b){
+        System.out.println(b/a);
+    }
+}
+
+/*
+    В методе main вызови метод div() с таким аргументом, чтобы вывод в консоли был "NaN".
+        Код метода div() изменять нельзя.
+
+        Требования:
+        •	В методе main() должен вызываться метод div().
+        •	Результатом вызова метода div() должен быть вывод в консоли "NaN".
+       •	Код метода div() не изменяй
+ */
