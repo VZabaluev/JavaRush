b8541a9acde85e67d3148882367b982cc2fec33d
VZabaluev <noname545@mail.ru>
2023-01-22 15:14:58 +0300

    Массив значений

---
+++ src/level8/task0710/Solution.java
@@ -0,0 +1,33 @@
+package level8.task0710;
+
+/*
+Массив значений
+*/
+
+import java.util.Arrays;
+
+public class Solution {
+    public static void main(String[] args) {
+        Integer[] array = new Integer[5];
+        fillArray(array, 3);
+        System.out.println(Arrays.toString(array));
+    }
+
+    public static void fillArray(Integer[] array, int value){
+        //напишите тут ваш код
+        // Arrays.fill(array, value); - альтернатива одной строкой
+        for (int i = 0; i < array.length; i++) {
+            array[i] = value;
+        }
+    }
+}
+
+/*
+Часто перед началом использования массива его необходимо заполнить значениями по умолчанию.
+Реализуй такое заполнение в методе fillArray(Integer[] array, int value).
+В качестве аргументов метод принимает массив и значение, которым его необходимо заполнить.
+Массив заполняется полностью, независимо от его длины.
+
+Требования:
+•	Метод fillArray(Integer[] array, int value) должен заполнять переданный в него массив значением value.
+*/
