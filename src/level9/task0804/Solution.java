7701d8bdd46054bdafac8c895c9614d6eb39b534
VZabaluev <noname545@mail.ru>
2023-01-29 20:31:04 +0300

    Randomizer от 0 до 99

---
+++ src/level9/task0804/Solution.java
@@ -0,0 +1,28 @@
+package level9.task0804;
+
+/*
+Randomizer
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(generateNumber());
+    }
+
+    public static int generateNumber() {
+        //напишите тут ваш код
+
+        return (int) (Math.random() * 99);
+    }
+}
+
+
+/*
+В этой задаче тебе предстоит реализовать метод generateNumber(), который будет возвращать случайное число от 0 до 99.
+В методе generateNumber() используй метод Math.random().
+
+Требования:
+•	В классе Solution должен быть публичный статический метод generateNumber() с типом возвращаемого значения int.
+•	Реализуй метод generateNumber() согласно условию.
+*/
